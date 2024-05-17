8431c7019e65d5f69eb5c2e5ef4e82aef87da0cb
Raditya Azka Prabaswara <18221152@mahasiswa.itb.ac.id>
2024-05-17 14:38:30 +0700

    add routing and change to typescript

---
+++ ppam-gymbro/screens/_layout.tsx
@@ -0,0 +1,6 @@
+import { Slot } from "expo-router";
+// import { useFonts } from 'expo-font';
+
+export default function AppLayout(){
+    return <Slot/>;
+}
---
+++ ppam-gymbro/screens/auth/_layout.tsx
@@ -0,0 +1,6 @@
+import { Slot, Stack } from "expo-router";
+// import { useFonts } from 'expo-font';
+
+export default function AppLayout(){
+    return <Stack/>;
+}
