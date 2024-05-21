18305d7d160f000716461333748b49f664efe029
Kean1202 <18221148@std.stei.itb.ac.id>
2024-05-21 15:44:37 +0700

    refined login page, ready for child pages

--- ppam-gymbro/app/auth/login/index.tsx
+++ ppam-gymbro/app/auth/login/index.tsx
@@ -1,49 +1,57 @@
-import { StyleSheet, Text, View, TextInput, ImageBackground} from 'react-native';
-import { Link } from "expo-router"
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, ImageBackground, ScrollView, Dimensions, KeyboardAvoidingView, Platform } from 'react-native';
+import { Link } from 'expo-router';
 import { Image } from 'expo-image';
 
 
-import React from 'react';
-
 export default function login_1() {
-  const [text, onChangeText] = React.useState('');
+  // const [text, onChangeText] = React.useState('');
 
   return (
     <View style={styles.layout}>
-      <ImageBackground source={require("../../../assets/Landing - Login.png")} style={styles.imageBackground}>
-        <View style={styles.LogoSpace}>
-          <Image
-            style = {styles.logo}
-            source = {require("../../../assets/icon.png")}
-            contentFit = "cover"
-          />
-          <Text style={styles.title}>Welcome</Text>
-          <Text style={styles.title}>Back</Text>
-        </View>
-        
-        <View style={styles.InputSpace}>
-          <TextInput
-            style={styles.InputBox}
-            placeholder="Username"
-            value={text}
-          />
+      <ScrollView contentContainerStyle={styles.scrollLayout}>
+        {/* View yang isinya logo + welcome back */}
+          <View style={styles.LogoSpace}>
+            <Image
+              style = {styles.logo}
+              source = {require("../../../assets/icon.png")}
+              contentFit = "contain"
+            />
+            <Text style={styles.title}>Welcome</Text>
+            <Text style={styles.title}>Back</Text>
+          </View>
+          
+          {/* View yang isinya input username dan password */}
+          <View style={styles.InputSpace}>
+            <TextInput
+              style={styles.InputBox}
+              placeholder="Username"
+              // value={text}
+            />
 
-          <TextInput
-            style={styles.InputBox}
-            placeholder="Password"
-            value={text}
-          />
+            <TextInput
+              style={styles.InputBox}
+              placeholder="Password"
+              // value={text}
+            />
 
-          <Text style={styles.subtext}>Forgot your<Link href="" style={styles.subtextBold}> password</Link>?</Text>
-        </View>
+            <Text style={styles.subtext}>Forgot your<Link href="" style={styles.subtextBold}> password</Link>?</Text>
+          </View>
+          
+          {/* View yang isinya tombol log in */}
+          <View style={styles.LowerThird}>
+            <Text style={styles.subtext}>Don't have an account? <Link href="" style={styles.subtextBold}>Register</Link></Text>
+            <WhiteButton text="Log In"></WhiteButton>
+          </View>
         
-        <View style={styles.LowerThird}>
-          <Text style={styles.subtext}>Don't have an account? <Link href="" style={styles.subtextBold}>Register</Link></Text>
-          {/* <Link href="" style={styles.WhiteButton}><Text style={styles.buttonText}>Log In</Text></Link> */}
-          <WhiteButton text="Log In"></WhiteButton>
-        </View>
-      </ImageBackground>
-    </View> 
+      </ScrollView> 
+
+      <ImageBackground 
+        source={require("../../../assets/Landing - Login.png")} 
+        style={[styles.fixed, styles.imageBackgroundContainer, {zIndex: -1}]}
+      />
+    </View>
+    
   )
 }
 
@@ -63,16 +71,36 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+
+  scrollLayout:{
+    flex: 1,
+    height: '100%',
+    width: '100%',
+  },
+
+  imageBackgroundContainer: {
+    width: Dimensions.get("window").width, //for full screen
+    height: Dimensions.get("window").height //for full screen
+  },
+  fixed: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
   },
 
   imageBackground: {
     flex: 1,
     height: "100%",
-    width: "100%"
+    width: "100%",
+    position: 'absolute',
   },
 
   LogoSpace: {
-    flex: 2,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 0
@@ -81,7 +109,6 @@ const styles = StyleSheet.create({
   logo: {
     height: 109,
     width: 109,
-    marginBottom: 4,
   },
 
   title:{
@@ -92,17 +119,16 @@ const styles = StyleSheet.create({
   },
 
   InputSpace:{
-    flex: 3,
+    flex: 2,
     width: "100%",
     height: "40%",
     alignItems: "center",
     justifyContent: "center",
-    marginBottom: 210,
     marginTop: 0
   },
 
   InputBox:{
-    height: "17.5%",
+    height: 65,
     width: "80%",
     margin: 6,
     borderWidth: 1,
@@ -127,9 +153,10 @@ const styles = StyleSheet.create({
   },
 
   LowerThird:{
-    paddingVertical: 30,
-    position: 'absolute',
-    bottom: 0,
+    // paddingVertical: 30,
+    // position: 'absolute',
+    // bottom: 0,
+    flex: 1,
     width: "100%",
     height: '20%',
     alignItems: "center",
@@ -138,7 +165,7 @@ const styles = StyleSheet.create({
 
   WhiteButton:{
     marginTop:4,
-    height: "60%",
+    height: 65,
     width: "80%",
     borderWidth: 1,
     borderRadius: 16,
